c828c5c9244e00a16e14c754aa17d67d361d4751
Ligrill <127509161+Ligrill@users.noreply.github.com>
2024-05-23 10:26:56 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var myButton = document.getElementById("myButton");
+
+
+myButton.addEventListener("click", function() {
+    
+    alert("björnar har förmågan att lösa problem och använda verktyg som exempelvis stenar eller stockar för att nå mat.");
+});
